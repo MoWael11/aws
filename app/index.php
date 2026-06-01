0a02ab1799c89bf73ca7d450207d0da55c742dfc
MoWael11 <midonit06@gmail.com>
2026-06-01 09:47:21 +0200

    feat(neted-stack): remove userdata & integrate secrets with db

--- app/index.php
+++ app/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Use this code snippet in your app.
  *
@@ -9,7 +10,18 @@
 require 'vendor/autoload.php';
 
 use Aws\SecretsManager\SecretsManagerClient;
+use Aws\Ssm\SsmClient;
 use Aws\Exception\AwsException;
+
+// 1. Ottieni il token (IMDSv2)
+$Token = exec('curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"');
+
+// 2. Recupera l’Availability Zone usando il token
+// NOTA: Usa le virgolette doppie "" e NON i backtick `` per la stringa del comando
+$az = exec("curl -s -H \"X-aws-ec2-metadata-token: $Token\" http://169.254.169.254/latest/meta-data/placement/availability-zone");
+$region = exec("curl -s -H \"X-aws-ec2-metadata-token: $Token\" http://169.254.169.254/latest/meta-data/placement/region");
+
+// 3. Stampa il risultato (es. eu-west-1a)
 
 /**
  * This code expects that you have AWS credentials set up per:
@@ -18,26 +30,38 @@ use Aws\Exception\AwsException;
 
 // Create a Secrets Manager Client
 $client = new SecretsManagerClient([
-    'version' => '2017-10-17',
-    'region' => 'us-east-1',
+  'version' => '2017-10-17',
+  'region' => $region,
 ]);
 
 $secret_name = 'db-instance-secret';
 
 try {
-    $result = $client->getSecretValue([
-        'SecretId' => $secret_name,
-    ]);
+  $result = $client->getSecretValue([
+    'SecretId' => $secret_name,
+  ]);
 } catch (AwsException $e) {
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    throw $e;
+  // For a list of exceptions thrown, see
+  // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+  throw $e;
 }
 
-// Decrypts secret using the associated KMS key.
+// Decrypt  s secret using the associated KMS key.
 $secret = $result['SecretString'];
 
 $secret_json = json_decode($secret, true);
+
+$ssmClient = new SsmClient([
+  'region' => $region,
+  'version' => 'latest'
+]);
+
+$tempCred = $ssmClient->getParameter([
+  'Name' => 'db-instance-parameter',
+  'WithDecryption' => true
+]);
+
+
 // Your code goes here
 // {"password":"V,`a}SgoB>BNTAaD{&4DDsxdm2`q|E","username":"postgres"}PHP Fatal error:  Uncaught TypeError: Cannot access offset of type string on string in /var/www/html/index.php:49
 // Stack trace:
@@ -45,42 +69,42 @@ $secret_json = json_decode($secret, true);
 //   thrown in /var/www/html/index.php on line 49
 // root@ip-10-0-3-160:/var/www/html#
 
-  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-  $dotenv->load();
-  
-  $host = $_ENV['DB_HOST'];
-  $port = 5432;
-  $db='postgres';
-  $password = $secret_json['password'];
-  $username = $secret_json['username'];
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-  $conn = null;
+$host = $_ENV['DB_HOST'];
+$port = 5432;
+$db = 'postgres';
+$password = $secret_json['password'];
+$username = $secret_json['username'];
 
-  try {
-    $conn = pg_connect(
-      "host=$host port=$port dbname=$db user=$username password=$password"
-    );
+$conn = null;
 
-    if (!$conn) {
-      die("Connection failed: " . pg_last_error());
+try {
+  $conn = pg_connect(
+    "host=$host port=$port dbname=$db user=$username password=$password"
+  );
 
-    echo "<h1>Connected to PostgreSQL successfully</h1>";
+  if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+  }
+  echo "<h1>Connected to PostgreSQL successfully</h1>";
 
-    $r = pg_query($conn, "SELECT NOW()");
+  $r = pg_query($conn, "SELECT NOW()");
 
-    if (!$r) {
-      die("Query failed: " . pg_last_error());
-    }
-
-    $row = pg_fetch_row($r);
-
-    echo "<p>Database time: " . $row[0] . "</p>";
-  } catch (Exception $e) {
-    echo "Database error: " . $e->getMessage();
-  } finally {
-    if ($conn) {
-      pg_close($conn);
-    }
+  if (!$r) {
+    die("Query failed: " . pg_last_error());
   }
 
-  ?>
+  $row = pg_fetch_row($r);
+
+  echo "<p>Database time: " . $row[0] . "</p>";
+} catch (Exception $e) {
+  echo "Database error: " . $e->getMessage();
+} finally {
+  if ($conn) {
+    pg_close($conn);
+  }
+}
+
+echo "from az: $az";
